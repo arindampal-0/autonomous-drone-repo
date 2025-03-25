0759177d4106eca9d3dfda244eadad8b30b52611
Arindam Pal <arindampal2000@gmail.com>
2025-03-26 00:05:34 +0530

    ptl: Changing to STABILIZE mode

--- pymavlink-takeoff-land/main.py
+++ pymavlink-takeoff-land/main.py
@@ -28,6 +28,36 @@ def main(args: list[str]) -> int:
     # make sure the connection is valid
     master.wait_heartbeat()
 
+    # choose a mode
+    mode = "STABILIZE"
+
+    ## check if mode is available
+    if mode not in master.mode_mapping():
+        print(f"Unknown mode: {mode}", file=sys.stderr)
+        print("Try: ", list(master.mode_mapping().keys()))
+        return 1
+
+    ## Get mode ID
+    mode_id = master.mode_mapping()[mode]
+
+    ## set mode message
+    master.mav.set_mode_send(
+        master.target_system,
+        master.target_component,
+        mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0,
+        0, mode_id, 0, 0, 0, 0, 0)
+    
+    ## Wait for ACK
+    while True:
+        ack_msg = master.recv_match(type="COMMAND_ACK", blocking=True, timeout=5)
+        ack_msg = ack_msg.to_dict()
+
+        if ack_msg["command"] != mavutil.mavlink.MAV_CMD_DO_SET_MODE:
+            continue
+
+        print(mavutil.mavlink.enums["MAV_RESULT"][ack_msg["result"]].description)
+        break
+
     return 0
 
 if __name__ == "__main__":
